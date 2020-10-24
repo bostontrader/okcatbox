0f45d1c976f949602f71e080e254dd5263abb6da
Thomas Radloff <bostontrader@gmail.com>
2020-10-24 09:32:58 +0800

    Fix

--- account-withdrawal-fee.go
+++ account-withdrawal-fee.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	utils "github.com/bostontrader/okcommon"
+	log "github.com/sirupsen/logrus"
 	"net/http"
 )
 
@@ -19,13 +20,53 @@ func generateWithdrawalFeeResponse(w http.ResponseWriter, req *http.Request, ver
 	if err {
 		return
 
+	}
+
+	// Check for the existence of a currency.
+	currencies, ok := req.URL.Query()["currency"]
+	if ok {
+		// Currency specified in the query string.  Is it valid?
+		txn := db.Txn(false)
+		defer txn.Abort()
+		raw, err := txn.First("withdrawalFees", "id", currencies[0])
+		if err != nil {
+			log.Fatalf("error: %v", err) // This should never happen
+			return
+		}
+		if raw == nil {
+			// Currency not valid, return 400 error
+			//setResponseHeaders(w, utils.ExpectedResponseHeaders, map[string]string{"Strict-Transport-Security": ""})
+			w.WriteHeader(400)
+			retVal, _ = json.Marshal(utils.Err30031(currencies[0]))
+			return retVal
+		} else {
+			// The currency is valid, return just that record.
+			retVal, _ = json.Marshal(raw)
+			return retVal
+		}
+
 	} else {
-		setResponseHeaders(w, utils.ExpectedResponseHeaders, map[string]string{"Strict-Transport-Security": "", "Vary": ""})
+		// No currency specified in the query string.  Return all records.
+		// Create read-only transaction
+		txn := db.Txn(false)
+		defer txn.Abort()
 
-		withdrawalFees := make([]utils.WithdrawalFee, 1)
-		withdrawalFees[0] = utils.WithdrawalFee{CurrencyID: "c", MinFee: "minf", MaxFee: "maxf"}
+		// Get all of the defined withdrawlFee records
+		it, err := txn.Get("withdrawalFees", "id") // id is an alias for CurrencyID
+		if err != nil {
+			log.Fatalf("error: %v", err) // This should never happen
+			return
+		}
+
+		var withdrawalFees []utils.WithdrawalFee
+
+		for obj := it.Next(); obj != nil; obj = it.Next() {
+			p := obj.(*utils.WithdrawalFee)
+			withdrawalFees = append(withdrawalFees, *p)
+		}
+
 		retVal, _ = json.Marshal(withdrawalFees)
-
+		return retVal
 	}
 
 	return
